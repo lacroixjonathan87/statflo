e7f7bbae0b6831cbae8be628a5864f21d1069ef6
Jonathan Lacroix <jonathan.lacroix@needls.com>
2018-10-25 10:23:37 -0400

    Add pagination tests

--- tests/Feature/UserIndexTest.php
+++ tests/Feature/UserIndexTest.php
@@ -39,9 +39,6 @@ class UserIndexTest extends TestCase
 		$data = $response->json('data');
 		$this->assertEquals(3, count($data));
 
-		$currentPage = $response->json('meta.current_page');
-		$this->assertEquals(1, $currentPage);
-
 		$total = $response->json('meta.total');
 		$this->assertEquals(3, $total);
 	}
@@ -59,11 +56,56 @@ class UserIndexTest extends TestCase
 		$data = $response->json('data');
 		$this->assertEquals(2, count($data));
 
+		$total = $response->json('meta.total');
+		$this->assertEquals(2, $total);
+	}
+
+	public function testRoleFilter()
+	{
+		factory(User::class)->create(['role' => 'Dentist']);
+		factory(User::class)->create(['role' => 'Doctor']);
+		factory(User::class)->create(['role' => 'Engineer']);
+
+		$response = $this->json('GET', '/api/users', ['role' => 'Do']);
+
+		$response->assertStatus(200);
+
+		$data = $response->json('data');
+		$this->assertEquals(1, count($data));
+
+		$total = $response->json('meta.total');
+		$this->assertEquals(1, $total);
+	}
+
+
+	public function testPagination()
+	{
+		factory(User::class, 20)->create();
+
+		$response = $this->json('GET', '/api/users');
+
+		$response->assertStatus(200);
+
+		$data = $response->json('data');
+		$this->assertEquals(15, count($data));
+
 		$currentPage = $response->json('meta.current_page');
 		$this->assertEquals(1, $currentPage);
 
 		$total = $response->json('meta.total');
-		$this->assertEquals(2, $total);
-		var_dump($response->json());
+		$this->assertEquals(20, $total);
+
+		$response = $this->json('GET', '/api/users', ['page' => 2]);
+
+		$response->assertStatus(200);
+
+		$data = $response->json('data');
+		$this->assertEquals(5, count($data));
+
+		$currentPage = $response->json('meta.current_page');
+		$this->assertEquals(2, $currentPage);
+
+		$total = $response->json('meta.total');
+		$this->assertEquals(20, $total);
 	}
 }
